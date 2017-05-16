f1a2233c156ef56eae1c2ca3c03fb606d1857e4d
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2017-05-16 18:29:09 +0700

    bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mercury',
-    version='0.4.1',
+    version='0.4.2',
     author='Rahmanda Wibowo',
     author_email='rahmandawibowo@gmail.com',
     url='https://github.com/rahmanda/mercury-py',
