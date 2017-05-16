8ddee097122f723f0c8904f4535b1ff95bd66f27
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2017-05-16 16:46:20 +0700

    fix filtering by json rule and add export to csv features

--- src/mercury_py/mercury.py
+++ src/mercury_py/mercury.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import print_function
-from builtins import str
-from builtins import int
 from builtins import input
 
 import sys
@@ -11,47 +9,61 @@ import csv
 import json
 import argparse
 
-global rule
-global clean_csv = []
-
-def filter_data_from_object(json_data, fil):
+def filter_data_from_dict(json_data, fil):
     cleaned_data = ''
-    if (fil['name'] in json_data) and ('hash_type' in fil):
-        if fil['hash_type'] == 'int':
-            cleaned_data = json_data[int(fil['name'])]
-        if (fil['content_type'] == 'int') and (cleaned_data != ''):
-            cleaned_data = int(cleaned_data)
+    if 'hash_type' in fil and fil['hash_type'] == 'int':
+        cleaned_data = json_data[int(fil['name'])]
+    if 'content_type' in fil and fil['content_type'] == 'int':
+        cleaned_data = int(cleaned_data)
     return cleaned_data
 
 def filter_data_from_list(json_data, filters):
-    cleaned_data = ''
     for fil in filters:
-        if type(fil) == str:
-            cleaned_data = json_data[fil]
-        else if type(fil) == dict:
-            cleaned_data = filter_data_from_object(json_data, fil)
-        else if type(fil) == list:
-            cleaned_data = filter_data_from_list(json_data, fil)
-    return cleaned_data
+        if type(fil) is str or type(fil) is unicode:
+            json_data = json_data[fil]
+        elif type(fil) is dict:
+            json_data = filter_data_from_dict(json_data, fil)
+        elif type(fil) is list:
+            json_data = filter_data_from_list(json_data, fil)
+    return json_data
 
-def extract_data_against_filter(raw_data, fil):
+def extract_data_against_filter(raw_data, rules):
     cleaned_data = []
     parsed_json = json.loads(raw_data)
-    for value in list(fil.items()):
-        cleaned_data.append(filter_data_from_list(parsed_json, [value]))
+    if 'entry_point' in rules:
+        parsed_json = filter_data_from_list(parsed_json, rules['entry_point'])
+    for fil in rules['filter']:
+        json_data = parsed_json
+        if type(fil) is str or type(fil) is unicode:
+            json_data = json_data[fil]
+        elif type(fil) is dict:
+            json_data = filter_data_from_dict(json_data, fil)
+        elif type(fil) is list:
+            json_data = filter_data_from_list(json_data, fil)
+        cleaned_data.append(json_data)
+    return cleaned_data
 
-def read_csv(csv_path):
+def read_csv(csv_path, delimiter, rules):
+    cleaned_csv = []
     with open(csv_path, 'rb') as csv_file:
-        reader = csv.reader(csv_file)
+        reader = csv.reader(csv_file, delimiter=delimiter)
         reader.next()
-        for key, value in list(rule.items()):
-            for row in reader:
+        for row in reader:
+            partial = []
+            for key, rule in list(rules.items()):
                 raw_data = row[int(key)]
-                clean_csv.append(extract_data_against_filter(raw_data, value))
+                partial += extract_data_against_filter(raw_data, rule)
+            cleaned_csv.append(partial)
+    return cleaned_csv
 
 def read_rule(rule_path):
+    rules = []
+    column_names = []
     with open(rule_path) as json_file:
-        rule = json.load(json_file)
+        json_rules = json.load(json_file)
+        rules = json_rules['rules']
+        column_names = json_rules['column_names']
+    return rules, column_names
 
 def export_to_csv_file(csv_list, export_path):
     with open(export_path, 'w') as output:
@@ -60,14 +72,16 @@ def export_to_csv_file(csv_list, export_path):
 
 def main():
     parser = argparse.ArgumentParser(description='Export JSON to CSV', prog='mercury-py')
-    parser.add_argument('--csv', help='Path of csv file', default=None, action='store')
-    parser.add_argument('--rule', help='JSON rule to extract json inside your csv file', default=None, action='store')
-    parser.add_argument('--export', help='Where you want to store exported data', default=None, action='store')
+    parser.add_argument('--csv', help='Path of csv file', default='example_csv.csv', action='store')
+    parser.add_argument('--csv-delimiter', help='delimiter', default=',', action='store')
+    parser.add_argument('--rule', help='JSON rule to extract json inside your csv file', default='example_rule.json', action='store')
+    parser.add_argument('--export', help='Where you want to store exported data', default='exported_json.csv', action='store')
 
     args = vars(parser.parse_args())
     csv_path = args['csv']
     rule_path = args['rule']
     export_path = args['export']
+    delimiter = str(args['csv_delimiter']).encode('utf-8')
 
     if not csv_path:
         csv_path = input('CSV File Path: ')
@@ -75,16 +89,14 @@ def main():
     if not rule_path:
         rule_path = input('JSON Rule File Path: ')
 
-    if not export_path:
-        export_path = 'exported_json.csv'
-
     if csv_path == '' or rule_path == '':
         print('Please enter csv path and rule path')
         sys.exit(1)
 
-    read_rule(rule_path)
-    read_csv(csv_path)
-    export_to_csv_file(clean_csv, export_path):
+    rules, column_names = read_rule(rule_path)
+    cleaned_csv = read_csv(csv_path, delimiter, rules)
+    cleaned_csv.insert(0, column_names)
+    export_to_csv_file(cleaned_csv, export_path)
 
 if __name__ == '__main__':
     main()
