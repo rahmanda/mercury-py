a396b49dfee66e3c1213bee312fd5703fda601df
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2017-05-16 17:42:40 +0700

    fix packaging, refer package directory explicitly on setup

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 from setuptools import setup
 
 setup(
-  name='mercury-py',
-  version='0.3.1',
-  author='Rahmanda Wibowo',
-  author_email='rahmandawibowo@gmail.com',
-  url='https://github.com/rahmanda/mercury-py',
-  scripts=['src/mercury-py']
+    name='mercury-py',
+    version='0.3.2',
+    author='Rahmanda Wibowo',
+    author_email='rahmandawibowo@gmail.com',
+    url='https://github.com/rahmanda/mercury-py',
+    scripts=['src/mercury-py'],
+    packages=['mercury_py'],
+    package_dir={'mercury_py': 'src/mercury-py'}
 )
